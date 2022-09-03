97ca578760d022cca11395e249858f1feff18321
Humam <104218563+humamul@users.noreply.github.com>
2022-09-03 13:10:08 +0530

    Update Student.java

--- Day3 JavaFrameworks/Student.java
+++ Day3 JavaFrameworks/Student.java
@@ -1,9 +1,13 @@
 package com.masai.app1;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 //it is entity or percistence class;
 @Entity
+@Table(name = "meriTable") // now the name of the table will be meriTable
+
 public class Student { 
 	
 	Student (){
@@ -17,10 +21,16 @@ public class Student {
 	}
 //Unique things
 
-	@Id
+	@Id   //it will be primary key based on it the update property in xml or there can be create in property so it will drop a table and create a new one again and again;
+	@Column(name= "sroll")//for any column of the table
 	private int roll;  //unique id
 	
+	
+	
+	
 	private int marks;
+	
+	
 	private String name;
 	public int getMarks() {
 		return marks;
