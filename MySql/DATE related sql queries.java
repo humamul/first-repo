f4932ce41a54a8f7b0db956675a81694277200f2
Humam Ul Islam <104218563+humamul@users.noreply.github.com>
2022-12-01 02:45:25 +0530

    Update DATE related sql queries.java

--- MySql/DATE related sql queries.java
+++ MySql/DATE related sql queries.java
@@ -1,2 +1,5 @@
 select * from orders where weekday(orderdate) = weekday(deliverydate) and weekday(orderdate) = weekday(shipdate);
 where you will get the output in the range from 0 to 6 i.e. mon to sun;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+select datediff(deliverydate,orderdate) df,count(orderid) from orders group by df order by df;
+another query gives the day difference between date
