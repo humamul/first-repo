417222b1ac3893f93cf60e1e4952952c1e75e452
Humam Ul Islam <104218563+humamul@users.noreply.github.com>
2022-12-01 04:14:49 +0530

    Update DATE related sql queries.java

--- MySql/DATE related sql queries.java
+++ MySql/DATE related sql queries.java
@@ -3,3 +3,6 @@ where you will get the output in the range from 0 to 6 i.e. mon to sun;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 select datediff(deliverydate,orderdate) df,count(orderid) from orders group by df order by df;
 another query gives the day difference between date
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+select Month(dateentered) m, count(customerid) c from customers group by m having m in(3,4,5) order by c desc;
+here starting of the month is from 1 o 12   and if you want from a particular year then add where extract(year from dateentered) = 2020 then group by m;
