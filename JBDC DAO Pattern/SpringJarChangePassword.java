efd43d9a7eaef3613829d637d0fb6037546df1bd
Humam Ul Islam <104218563+humamul@users.noreply.github.com>
2023-01-14 12:07:04 +0530

    Create SpringJarChangePassword.java

---
+++ JBDC DAO Pattern/SpringJarChangePassword.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+@Autowired
+Environment env;
+
+@Bean
+public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+    dataSource.setUrl(env.getProperty("spring.datasource.url"));
+    dataSource.setUsername(env.getProperty("spring.datasource.username"));
+    dataSource.setPassword(env.getProperty("spring.datasource.password", "defaultpassword"));
+    return dataSource;
+}
